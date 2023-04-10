889dd8de68777d4ca8b83ea57fa5209b638e5d5b
Adriano <adriano_alves_2008@hotmail.com>
2023-04-09 22:05:53 -0300

    Criando rota de admin

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
 
-function Dashboard() {
+import { getSession } from 'next-auth/react'
+
+export default function Dashboard() {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,4 +15,18 @@ function Dashboard() {
   )
 }
 
-export default Dashboard
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
