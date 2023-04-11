1318814d328242d259086a32b0b551731e4d0e3d
Adriano <adriano_alves_2008@hotmail.com>
2023-04-11 19:27:38 -0300

    Listando itens

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next'
-import { ChangeEvent, useState, FormEvent } from 'react'
+import { ChangeEvent, useState, FormEvent, useEffect } from 'react'
 import styles from './styles.module.css'
 import Head from 'next/head'
 
@@ -10,7 +10,14 @@ import { FiShare2 } from 'react-icons/fi'
 import { FaTrash } from 'react-icons/fa'
 
 import { db } from '../../services/firebaseConnection'
-import { addDoc, collection } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot
+} from 'firebase/firestore'
 
 interface HomeProps {
   user: {
@@ -18,9 +25,48 @@ interface HomeProps {
   }
 }
 
+interface TaskProps {
+  id: string
+  created: Date
+  public: boolean
+  tarefa: string
+  user: string
+}
+
 export default function Dashboard({ user }: HomeProps) {
   const [input, setInput] = useState('')
   const [publicTask, setPublicTask] = useState(false)
+  const [tasks, setTasks] = useState<TaskProps[]>([])
+
+  useEffect(() => {
+    async function loadTarefas() {
+      const tarefasRef = collection(db, 'tarefas')
+
+      const q = query(
+        tarefasRef,
+        orderBy('created', 'desc'),
+        where('user', '==', user?.email)
+      )
+
+      onSnapshot(q, snapshot => {
+        let lista = [] as TaskProps[]
+
+        snapshot.forEach(doc => {
+          lista.push({
+            id: doc.id,
+            tarefa: doc.data().tarefa,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public
+          })
+        })
+
+        setTasks(lista)
+      })
+    }
+
+    loadTarefas()
+  }, [user?.email])
 
   function handleChangePublic(e: ChangeEvent<HTMLInputElement>) {
     setPublicTask(e.target.checked)
@@ -85,20 +131,24 @@ export default function Dashboard({ user }: HomeProps) {
         <section className={styles.taskContainer}>
           <h1>Minhas tarefas</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>Público</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo</p>
-              <button className={styles.trashButton}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+          {tasks.map(item => (
+            <article key={item.id} className={styles.task}>
+              {item.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>Público</label>
+                  <button className={styles.shareButton}>
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div>
+              )}
+              <div className={styles.taskContent}>
+                <p>{item.tarefa}</p>
+                <button className={styles.trashButton}>
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div>
