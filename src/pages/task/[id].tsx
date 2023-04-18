c5dec1a4c4342cb76521284a8a6518de9b502114
Adriano <adriano_alves_2008@hotmail.com>
2023-04-18 18:42:02 -0300

    Detalhes da tarefa pt2

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -4,17 +4,39 @@ import { GetServerSideProps } from 'next'
 
 import { db } from '../../services/firebaseConnection'
 import { doc, collection, query, where, getDoc } from 'firebase/firestore'
+import Textarea from '@/src/components/Textarea'
 
-export default function Task() {
+interface TaskProps {
+  item: {
+    tarefa: string
+    created: string
+    public: boolean
+    user: string
+    taskId: string
+  }
+}
+
+export default function Task({ item }: TaskProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Detalhes da tarefa</title>
       </Head>
 
       <main className={styles.main}>
         <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item.tarefa}</p>
+        </article>
       </main>
+
+      <section className={styles.comentsContainer}>
+        <h2>Fazer comentário</h2>
+        <form>
+          <Textarea placeholder="Digite o seu comentário..." />
+          <button className={styles.button}>Enviar comentário</button>
+        </form>
+      </section>
     </div>
   )
 }
@@ -55,6 +77,8 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 
   return {
-    props: {}
+    props: {
+      item: task
+    }
   }
 }
